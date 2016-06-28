696da5190ad9c543cd6df39eb1d123dffad29936
Елизавета Каткова <ryu-chan@yandex.ru>
2016-06-28 12:29:39 +0400

    Поправила js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 function toggleMenuFunction() {
     var header = document.getElementById("header");
-    header.classList.toggle("menu-opened")
+    header.classList.toggle("menu-opened");
 }
+
+function checkMenuAdded()
+{
+    if (document.getElementById("header"))
+    {
+        toggleMenuFunction();
+        return;
+    }
+    setTimeout(checkMenuAdded, 100);
+}
+
+checkMenuAdded();
